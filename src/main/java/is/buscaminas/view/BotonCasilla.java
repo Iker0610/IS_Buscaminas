673c7eac96c7e52f2e141c0d7cb90029a5bccfd6
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2020-03-23 19:58:19 +0100

    VistaContador

--- src/main/java/is/buscaminas/view/BotonCasilla.java
+++ src/main/java/is/buscaminas/view/BotonCasilla.java
@@ -3,7 +3,6 @@ package is.buscaminas.view;
 
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundSize;
---
+++ src/main/java/is/buscaminas/view/VistaContador.java
@@ -0,0 +1,28 @@
+package is.buscaminas.view;
+
+import javax.swing.text.html.ImageView;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class VistaContador extends javafx.scene.Node implements PropertyChangeListener {
+    ImageView cifra1;
+    ImageView cifra2;
+    ImageView cifra3;
+    ImageView cifra4;
+
+    public VistaContador ()
+    {
+
+    }
+
+    @Override
+    public void propertyChange (PropertyChangeEvent propertyChangeEvent)
+    {
+        //TODO
+    }
+
+    private void actualizarContador (int pTiempo)
+    {
+        //TODO
+    }
+}
