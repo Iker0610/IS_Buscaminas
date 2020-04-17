7f9367bff176d18587c9e8a9469b231bf13e9f19
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2020-04-17 19:35:49 +0200

    Ajustes de la ayuda: se ha eliminado la parte de model y limpiado el código

--- src/main/java/is/buscaminas/controller/MenuAyudaController.java
+++ src/main/java/is/buscaminas/controller/MenuAyudaController.java
@@ -1,16 +1,63 @@
 package is.buscaminas.controller;
 
 
-import is.buscaminas.model.Ayuda;
+import is.buscaminas.view.VistaAyuda;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.File;
 
 public class MenuAyudaController {
+    // Atributos
+    private int pagTotal;
+    private int pagAct;
 
-    //Metodos de eventos
+    // Elementos FXML
     @FXML
-    private void nextAyuda(ActionEvent pEvento) { Ayuda.getAyuda().siguiente(); }
+    private VistaAyuda panelAyuda;
 
     @FXML
-    private void prevAyuda(ActionEvent pEvento) { Ayuda.getAyuda().anterior(); }
+    private Button btnNext;
+
+    @FXML
+    private Button btnPrev;
+
+    // Construtora
+    @FXML
+    private void initialize ()
+    {
+        pagAct = 1;
+        pagTotal = getNumPagTotal();
+        btnPrev.setDisable(true);
+    }
+
+    // Metodos
+    private int getNumPagTotal ()
+    {
+        //Pre:
+        //Post: Devuelve el número de páginas de ayuda que tenemos
+
+        File[] paginas = new File("src/main/resources/is/buscaminas/ui/ayuda").listFiles();
+        return paginas != null ? paginas.length : 0;
+    }
+
+    // Metodos de eventos
+    @FXML
+    private void nextAyuda (ActionEvent pEvento)
+    {
+        pagAct++;
+        panelAyuda.cambiarPaginaAyuda(pagAct);
+        if (pagAct == pagTotal) btnNext.setDisable(true);
+        if (pagAct > 1) btnPrev.setDisable(false);
+    }
+
+    @FXML
+    private void prevAyuda (ActionEvent pEvento)
+    {
+        pagAct--;
+        panelAyuda.cambiarPaginaAyuda(pagAct);
+        if (pagAct == 1) btnPrev.setDisable(true);
+        if (pagAct < pagTotal) btnNext.setDisable(false);
+    }
 }
--- src/main/java/is/buscaminas/view/VistaAyuda.java
+++ src/main/java/is/buscaminas/view/VistaAyuda.java
@@ -1,75 +1,29 @@
 package is.buscaminas.view;
 
-import is.buscaminas.model.Ayuda;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
-
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.File;
 
-public class VistaAyuda extends Pane implements PropertyChangeListener {
-
-    //Atributos
-    private static double anch = imgAnch(1); //tamaño de la primera imagen (todas las imagenes tendrán el mismo tamaño)
-    private static double alt = imgAlt(1);
-
-
+public class VistaAyuda extends Pane {
 
     // La Clase que muestra las páginas de ayuda
     public VistaAyuda(){
         super();
-        //tamaño del pane
-        setMinSize(anch,alt);
-
-        //añadimos esta vista como observer de Ayuda
-        Ayuda.getAyuda().addObserver(this::cambiarPanel);
 
         //Ponemos la primera imagen
-        cambiarImagen(1);
+        cambiarPaginaAyuda(1);
     }
 
-
-
-    //para obtener tamaños de imagenes:
-
-    private static double imgAnch(int i) {
-        Image imagen = new Image(new File("src/main/resources/is/buscaminas/ui/ayuda/" + i + ".png").toURI().toString());
-        return imagen.getWidth();
-    }
-
-
-    private static double imgAlt(int i) {
-        Image imagen = new Image(new File("src/main/resources/is/buscaminas/ui/ayuda/" + i + ".png").toURI().toString());
-        return imagen.getHeight();
-    }
-
-    //más metodos
-
-    private void cambiarPanel (PropertyChangeEvent propertyChangeEvent) {
-        //Pre: Un evento con un número
-        //Post: Se ha cambia el menu de ayuda actual dependiendo del int recibido
-
-        int pagAct = (int) propertyChangeEvent.getNewValue();
-        cambiarImagen(pagAct);
-
-    }
-
-    private void cambiarImagen(int pPag){
+    public void cambiarPaginaAyuda(int pPag){
         //Pre: un numero
         //Post: Se carga la pagina correspondiente a ese numero
 
         //cargamos la imagen
-        Image imagenCasilla = new Image(new File("src/main/resources/is/buscaminas/ui/ayuda/" + pPag + ".png").toURI().toString());
-        BackgroundSize backgroundSize = new BackgroundSize(anch, alt, false, false, false, false);
+        Image imagenCasilla = new Image(new File("src/main/resources/is/buscaminas/ui/ayuda/ayudaPag" + pPag + ".png").toURI().toString());
+        BackgroundSize backgroundSize = new BackgroundSize(1250, 1250, false, false, true, true);
         setBackground(new Background(new BackgroundImage(imagenCasilla, null, null, null, backgroundSize )));
     }
-
-    //método necesario para el observer
-    @Override
-    public void propertyChange(PropertyChangeEvent propertyChangeEvent) { }
-
 }
