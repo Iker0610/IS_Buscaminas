852f233991cc508ce324216a734d5359645894a1
alexanderplatas <48363345+alexanderplatas@users.noreply.github.com>
2020-05-07 12:21:29 +0200

    ++

--- src/main/java/is/buscaminas/controller/VentanaRankingController.java
+++ src/main/java/is/buscaminas/controller/VentanaRankingController.java
@@ -1,11 +1,15 @@
 package is.buscaminas.controller;
 
+import is.buscaminas.model.estructurasDatos.Node;
+import is.buscaminas.model.estructurasDatos.OrderedDoubleLinkedList;
+import is.buscaminas.model.ranking.JugadorRanking;
+import is.buscaminas.model.ranking.Ranking;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class VentanaRankingController{
-    
+
     @FXML
     private Button aceptar;
 
